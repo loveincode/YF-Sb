77ed4aa7f6d98523444d2bc7d8ccf4802c3fc4e4
huyf <huyifan@txp-life.com>
2019-05-24 11:04:40 +0800

    add logback and banner

--- YF-web/src/main/java/com/loveincode/YFWebApplication.java
+++ YF-web/src/main/java/com/loveincode/YFWebApplication.java
@@ -1,5 +1,6 @@
 package com.loveincode;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
--- YF-web/src/main/java/com/loveincode/controller/UserController.java
+++ YF-web/src/main/java/com/loveincode/controller/UserController.java
@@ -5,6 +5,7 @@ import com.google.gson.GsonBuilder;
 import com.loveincode.feign.GitHubClient;
 import com.loveincode.UserService;
 import com.loveincode.common.ResultDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
  * @date :2019-05-23
  * com.loveincode.controller
  */
+@Slf4j
 @RestController
 @RequestMapping("/user/*")
 public class UserController {
@@ -28,7 +30,12 @@ public class UserController {
     @GetMapping("hello")
     public ResultDTO list() {
         String value = gitHubClient.searchRepo("21");
+        log.info("123");
+        for (int i = 1; i < 10000; i++) {
+            log.info("" + i);
+        }
         return ResultDTO.ofSuccess(GSON.toJson(value));
+
 
     }
 
